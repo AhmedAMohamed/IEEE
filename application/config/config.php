1a78986ee6db8274a665df1fb9389d223c9bd617
Mohamed Essam <mohamedessamfathalla@gmail.com>
2015-02-15 00:56:08 +0200

    change in config.php and add folder libs in main
    change in config.php with adding server name ,libs folder for js and css
    libs besides application main folder

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '/IEEE/';
+$config['base_url']	='http://' . $_SERVER['SERVER_NAME'] . '/IEEE/';
 
 /*
 |--------------------------------------------------------------------------
