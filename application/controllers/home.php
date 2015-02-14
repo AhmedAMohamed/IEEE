f8eef7df1065e51ea29a5a2363293f00fad9d16b
Mohamed Essam <mohamedessamfathalla@gmail.com>
2015-02-15 00:53:17 +0200

    add header nav bar , change in url base in config

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -19,7 +19,8 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+		$this->load->view('header');
 		$this->form();
 	}
 	public function form()
---
+++ application/views/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <title>IEEE ASU Student Branch</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="<?=base_url();?>libs/bootstrap.min.css"
+    rel="stylesheet">
+    <script type="text/javascript" src="<?=base_url();?>libs/jquery.min.js"></script>
+    <script type="text/javascript" src="<?=base_url();?>libs/bootstrap.min.js"></script>
+    <style type="text/css">
+    .navbar-default .navbar-nav>li>a {
+        color: #fff;
+    }
+    .navbar-inverse .navbar-nav>.active>a {
+      color: #0072A6;
+      background-color: #fff;
+    }
+    </style>
+  </head>
+  
+  <body>
+    <div class="navbar navbar-default navbar-static-top navbar-inverse" style="background-color:#0072A6">
+      <div class="container">
+        <div class="navbar-header">
+          <a type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"></a>
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <a class="navbar-brand" href="http://ieeeasusb.com/" style="color:white">IEEE ASU Student Branch</a>
+        </div>
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active">
+              <a href="#">Home</a>
+            </li>
+           
+          </ul>
+        </div>
+      </div>
+    </div>
