509f0e360ca35f0861bccc97f66cdd225ee8da0c
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-02-20 01:34:35 +0200

    gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'ieee';
+$db['default']['hostname'] = 'ieeeasusbcom.ipagemysql.com';
+$db['default']['username'] = 'ieeeportal2';
+$db['default']['password'] = 'portal2';
+$db['default']['database'] = 'portal';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
